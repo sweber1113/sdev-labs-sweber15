a83c00efa769ed58ed39430f6c4a9c095150ce62
sweber1113 <79806638+sweber1113@users.noreply.github.com>
2021-05-09 20:17:57 -0500

    Add files via upload

--- class_projects/delete_bms.php
+++ class_projects/delete_bms.php
@@ -36,18 +36,4 @@
 
   display_user_menu();
   do_html_footer();
-  }
-  
-  function delete_bm($user, $url) {
-	// delete one URL from the database
-	$conn = db_connect();
-  
-	// delete the bookmark
-	if (!$conn->query("delete from bookmark where
-                    username='".$user."'
-                    and bm_url='".$url."'")) {
-     throw new Exception('Bookmark could not be deleted');
-  }
-  return true;
-}
 ?>
